ef74964110eebaa2aabf71060985edd0c56b325f
zhouzhihu <web_study2004@163.com>
2017-01-20 14:56:22 +0800

    add static js

--- meta.js
+++ meta.js
@@ -75,7 +75,7 @@ module.exports = {
     ".eslintrc.js": "lint",
     ".eslintignore": "lint",
     "build/vue.cfg.js": "frontFrame == 'vue'",
-    "static": "frontFrame == 'angularJS'"
+    "static/": "frontFrame == 'angularJS'"
   },
   "completeMessage": "To get started:\n\n  cd {{destDirName}}\n  npm install\n  npm run dev"
 };
